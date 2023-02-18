ab2b89259b2dd39ac71646355581c567443ae0e7
dawodahmadi <ahmadi.dawod@gmail.com>
2023-02-18 10:08:14 -0500

    added some codes

--- src/main/java/com/prog2/labs/LabFour.java
+++ src/main/java/com/prog2/labs/LabFour.java
@@ -1,4 +1,7 @@
 package com.prog2.labs;
+
+import java.util.Arrays;
+
 /**
  * @author adinashby
  *
@@ -23,6 +26,31 @@ public class LabFour {
     }
     
     public int threeSumClosest(int[] nums, int target) {
-        return -1;
+         Arrays.sort(nums);
+        int closestSum = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+                if (sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    return closestSum;
+                }
+            }
+        }
+        return closestSum;
     }
+
+    
+
+
+
+
+    
 }
